f94897f168138546e689f4acc3c8eae629f85dc5
Zachary Szeto <60451103+zachszeto@users.noreply.github.com>
2022-11-09 11:49:27 -0500

    Update szeto-z-hw2.rkt

--- szeto-z-hw2.rkt
+++ szeto-z-hw2.rkt
@@ -2,7 +2,6 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname szeto-z-hw2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
 ;Zachary Szeto zdszeto
-;Ian Kane ibkane
 
 ;Problem 1
 (define-struct tornado (scale distance max-winds))
